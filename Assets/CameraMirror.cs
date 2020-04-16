4bbbb6a9b5ac1783953513e2ba2411bc8df94c99
Dmitry <dmitry.dodonkin@westwing.ru>
2020-04-16 18:07:30 +0300

    Created working mirrors (nesting not working)

---
+++ Assets/CameraMirror.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMirror : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnPreCull()
+    {
+        foreach (MirrorPlane mirror in MirrorPlane.mirrors)
+        {
+            mirror.Render();
+        }
+    }
+}
---
+++ Assets/TestPlane.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlane : MonoBehaviour
+{
+    Camera source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source = GameObject.Find("MirrorCamera").GetComponent<Camera>();
+        RenderTexture t = new RenderTexture(Screen.width, Screen.height, 0);
+        source.targetTexture = t;
+        GetComponent<MeshRenderer>().material.SetTexture("_MainTex", t);
+    }
+}
