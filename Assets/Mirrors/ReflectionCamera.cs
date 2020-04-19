6e6657db7a8ec2d61365492d7c5cb61eab44e82d
Dmitry <dmitry.dodonkin@westwing.ru>
2020-04-19 16:44:30 +0300

    Fixed reflection flip when main camera is flipped

---
+++ Assets/Mirrors/ReflectionCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionCamera : MonoBehaviour
+{
+    private void OnPreCull()
+    {
+        Camera camera = gameObject.GetComponent<Camera>();
+        camera.ResetWorldToCameraMatrix();
+        camera.ResetProjectionMatrix();
+        Vector3 scale = new Vector3(Game.Current().isFlipped ? -1 : 1, 1, 1);
+        camera.projectionMatrix = camera.projectionMatrix * Matrix4x4.Scale(scale);
+    }
+
+    void OnPreRender()
+    {
+        GL.invertCulling = Game.Current().isFlipped;
+    }
+
+    void OnPostRender()
+    {
+        GL.invertCulling = false;
+    }
+}
