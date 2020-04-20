f9070293f4710ec91f71c349c1d2d5bf9077ec8e
8Xenon8 <dmitry.dodonkin@westwing.ru>
2020-04-21 02:58:17 +0300

    minor fixes

--- Assets/Mirrors/MirrorPlane.cs
+++ Assets/Mirrors/MirrorPlane.cs
@@ -27,12 +27,8 @@ public class MirrorPlane : MonoBehaviour
 
         planeObj = transform.gameObject;
 
-        Mesh m = GetComponent<MeshFilter>().mesh;
-        plane = new Plane(
-            m.vertices[0],
-            m.vertices[1],
-            m.vertices[2]
-        );
+        Vector3 mirrorsNormal = planeObj.transform.localRotation * new Vector3(0f, 1, 0f);
+        plane = new Plane(mirrorsNormal, planeObj.transform.position);
 
         viewport = Camera.main;
         mirrors.Add(this);
@@ -53,30 +49,21 @@ public class MirrorPlane : MonoBehaviour
 
         source.transform.position = planeObj.transform.localToWorldMatrix.MultiplyPoint(reflectionCameraPosition);
 
-        Vector3 mirrorsNormal = planeObj.transform.localRotation * new Vector3(0f, 1, 0f);
-        Plane planeOfMirror = new Plane(mirrorsNormal, planeObj.transform.position);
-
         float intersectionDistance;
 
         Ray rayToMirror = new Ray(viewport.transform.position, viewport.transform.forward);
-        if (planeOfMirror.Raycast(rayToMirror, out intersectionDistance))
+        if (plane.Raycast(rayToMirror, out intersectionDistance))
         {
             Vector3 hitPoint = rayToMirror.GetPoint(intersectionDistance);
             source.transform.LookAt(hitPoint);
         } else {
             rayToMirror = new Ray(viewport.transform.position, viewport.transform.forward * -1);
-            planeOfMirror.Raycast(rayToMirror, out intersectionDistance);
+            plane.Raycast(rayToMirror, out intersectionDistance);
             Vector3 hitPoint = rayToMirror.GetPoint(intersectionDistance);
-
-            Debug.DrawLine(hitPoint - Vector3.left, hitPoint + Vector3.left);
-            Debug.DrawLine(hitPoint - Vector3.forward, hitPoint + Vector3.forward);
 
             source.transform.LookAt(hitPoint);
             Vector3 dir = source.transform.position - hitPoint;
-            Debug.DrawLine(hitPoint, hitPoint + dir * 10, Color.green);
             source.transform.LookAt(source.transform.position + dir);
-            //source.transform.rotation *= Quaternion.Euler(0, -1, 0);
-            //source.transform.rotation = Quaternion. (source.transform.rotation);
         }
 
         source.Render();
@@ -84,19 +71,9 @@ public class MirrorPlane : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        Vector3 p = gameObject.GetComponent<Collider>().ClosestPoint(other.gameObject.transform.position);
-
-        Vector3 diff = other.gameObject.transform.position - p;
-
-        Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
-
-        //other.gameObject.GetComponent<Rigidbody>().AddForce(diff * (1 - diff.magnitude) * 300, ForceMode.Force);
-        //otherRb.velocity *= diff.magnitude;
-        //otherRb.AddForce(diff * (1 - diff.magnitude) * 10, ForceMode.Force);
-
-        if (canSwap && diff.magnitude < 1f)
+        if (canSwap && plane.GetSide(other.gameObject.transform.position) == false)
         {
-            Swap(otherRb);
+            Swap(other.gameObject.GetComponent<Rigidbody>());
             canSwap = false;
         }
     }
@@ -108,7 +85,7 @@ public class MirrorPlane : MonoBehaviour
 
     private void Swap(Rigidbody rb)
     {
-        rb.velocity *= -1;
+        rb.velocity = Vector3.Reflect(rb.velocity.normalized, plane.normal) * rb.velocity.magnitude;
         Game.Current().MirrorSwap();
         Camera.main.gameObject.GetComponent<CameraFollow>().FlipCamera();
     }
